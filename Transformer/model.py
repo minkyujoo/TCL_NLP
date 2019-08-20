9839b72bae244aa1061ff4780ed7311a4dd97c41
Minkyu Joo <joo.minkyu@gmail.com>
2019-08-20 13:56:23 +0900

    Create model.py

---
+++ Transformer/model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import sys
+
+from configs import DEFINES
+import numpy as np
+
+def layer_norm(inputs, eps=1e-6):
+  feature_shape = inputs.get_shape()[-1:]
+  mean = tf.keras.backend.mean(inputs, [-1], keepdims=True)
+  std = tf.keras.backend.std(inputs,[-1], keepdims=True)
+  beta = tf.get_variable("beta", initialize=tf.zeros(feature_shape))
+  gamma = tf.get_variable("gamma", initialize=tf.zeros(feature_shape))
+  return gamma * (inputs-mean) / (std+eps) + beta
+
